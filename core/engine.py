69e032c9339051a69aa950dcd0d9a2a93a024e66
kmiecikelo <94751425+kmiecikelo@users.noreply.github.com>
2025-06-11 20:04:46 +0200

    poprawienie menu w pętli gry

--- core/engine.py
+++ core/engine.py
@@ -1,20 +1,20 @@
+import time
+
 from colorama import init, Fore, Style
-from scenes.intro import create_player
-from core.player import Player
-init(autoreset=True, convert=True)
+from utils.clean_screen import clear
+
 
 def game_loop(player):
     print("\n" + Fore.CYAN + "=" * 23 + " MENU " + "=" * 23)
-    print("Co chcesz zrobić?")
     print("stat / statystyki / info – pokaż status gracza")
-    print("stat / statystyki / info – pokaż status gracza")
+    print("zapisz / save - zapisz stan gry")
     print("\n" * 2 + Fore.CYAN + "=" * 23 + " KODY " + "=" * 23)
     print("exp100 – dodaj 100 expa")
     print("exp1000 – dodaj 1000 expa")
 
-    wybor = input(f"\nWpisz komendę: ").lower().strip()
+    wybor = input(f"\n<hp{player.hp}/{player.max_hp}> <g{player.gold}> <exp{player.exp}/{player.exp_to_next_level()}> >> ").lower().strip()
 
-    if wybor in ["stat", "statystyki", "info", "informacje"]:
+    if wybor in ["stat", "statystyki", "info", "informacje", "stats"]:
         player.status()
     elif wybor == "exp100":
         player.gain_exp(100)
@@ -23,5 +23,9 @@ def game_loop(player):
     elif wybor in ["zapisz", "save"]:
         player.save()
         print(Fore.GREEN + "Gra zapisana!")
+        time.sleep(0.8)
+        clear()
     else:
         print(Fore.RED + "Nieprawidłowa komenda.")
+        time.sleep(0.8)
+        clear()
