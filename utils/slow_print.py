f90e98020058d7d30a3cfc7cef27b2854951684f
kmiecikelo <94751425+kmiecikelo@users.noreply.github.com>
2025-06-11 17:56:01 +0200

    Update slow_print.py

--- utils/slow_print.py
+++ utils/slow_print.py
@@ -1,9 +1,9 @@
+from colorama import init, Fore, Style
 import time
-import sys
+init(autoreset=True, convert=True)
 
 def slow_print(text, delay=0.08):
     for char in text:
-        sys.stdout.write(char)
-        sys.stdout.flush()
+        print(char, end='', flush=True)
         time.sleep(delay)
     print()
